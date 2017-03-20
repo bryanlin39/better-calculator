44d67b916561eda9f617ded3e68ee554e20e889f
Benjamin Petrosky and Ilene Gorski <ilene@>
2017-03-20 13:22:57 -0700

    Un-comment majority of code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,45 @@
-// var add = function(number1, number2) {
-//   return number1 + number2;
-// };
-//
-// var number1 = parseInt(prompt("Enter a number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-//
-// alert(add(number1, number2));
-//
-// var multiply = function(number1, number2) {
-//   return number1 * number2;
-// };
-//
-// var number1 = parseInt(prompt("Enter a number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-//
-// alert(multiply(number1, number2));
-//
-// var sub = function(number1, number2) {
-//   return number1 - number2;
-// };
-//
-// var number1 = parseInt(prompt("Enter a number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-//
-// alert(sub(number1, number2));
-//
-// var divide = function(number1, number2) {
-//   return number1 / number2;
-// };
-//
-// var number1 = parseInt(prompt("Enter a number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-//
-// alert(divide(number1, number2));
-//
-// var height = parseInt(prompt("Enter your height in inches"));
-// var weight = parseInt(prompt("Enter your weight in pounds"));
-// weight=weight*.45;
-// height = Math.pow((height*.025), 2);
-// var bmi = weight/height;
-// alert(bmi);
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(add(number1, number2));
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(multiply(number1, number2));
+
+var sub = function(number1, number2) {
+  return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(sub(number1, number2));
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+alert(divide(number1, number2));
+
+var height = parseInt(prompt("Enter your height in inches"));
+var weight = parseInt(prompt("Enter your weight in pounds"));
+weight=weight*.45;
+height = Math.pow((height*.025), 2);
+var bmi = weight/height;
+alert(bmi);
 
 var Celcius= parseInt(prompt("Enter a temperature in Celcius"));
 Farenheit= Celcius*9/5 + 32;
